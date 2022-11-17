a21f3278b7c7673768defd7dd0a016813b6c7ac0
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-11-17 09:53:57 +0100

    add function to fileSelector

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego n
 "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
 
 console.log( txt.split(/[\r\n]+/gm) );
+
+const fileSelector = document.querySelector('.uploader__input');
+
+fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+  });
